b80438b815e921b8f4e196b000b69c35e6c3710f
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-05-04 18:30:39 -0300

    communication with FTP protocol

--- comunicationProtocols/socket.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-response = 'Y'
-while response=='Y':
-    url = input("Type a URL:")
-    ip = socket.gethostbyname(url)
-    print("The reference IP of passed URL is:", ip)
-    response=input("Type <Y> to continue:").toUpper()
