6bcee17c322744e44a855f90f1e2e9dbdcf6e881
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-05-04 18:48:52 -0300

    correction of value type of port

--- comunicationProtocols/serverApplicationUDP.py
+++ comunicationProtocols/serverApplicationUDP.py
@@ -1,7 +1,7 @@
 from socket import *
 
 server = "127.0.0.1"
-port = "43210"
+port = 43210
 
 obj_socket = socket(AF_INET, SOCK_DGRAM)
 obj_socket.bind((server, port))
@@ -12,6 +12,6 @@ while True:
     print("Origin: ", origin)
     print("Received data: ", data.decode())
     response = input("Type your response:")
-    obj_socket.sendTo(response.encode(), origin)
+    obj_socket.sendto(response.encode(), origin)
 
 obj_socket.close()
