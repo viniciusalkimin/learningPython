2262b5f0459f906a60bc8a7c8170ac0c1b2c2e78
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-06-05 17:19:53 -0300

    Refactoring algorithm

--- selectionSearch/src/selectionSearch.py
+++ selectionSearch/src/selectionSearch.py
@@ -2,16 +2,16 @@ arr = [4,5,8,2,9,1]
 
 def lowerNumber(arr):
     for a in range(len(arr)):
-        lower = arr[a]
-        indice = a
+        lower = a
+       # indice = a
         for i in range(a + 1, len(arr)):
-            if arr[i] < lower:
-                lower = arr[i]
-                indice = i
+            if arr[i] < arr[lower]:
+                lower = i
+                #indice = i
 
         other = arr[a]
-        arr[a] = lower
-        arr[indice] = other
+        arr[a] = arr[lower]
+        arr[lower] = other
 
 lowerNumber(arr)
 
