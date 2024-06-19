6a933c3622bc80791958f7f0992c908bc7535a11
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-06-19 19:54:38 -0300

    factorial without recursion

--- recursionStack/factorial.py
+++ recursionStack/factorial.py
@@ -3,5 +3,12 @@ def factorialStack(number):
         return 1
     else:
         return number * factorialStack(number - 1)
+def otherFactorial(num):
+    total = num
+    while num > 1:
+        total = total*(num-1)
+        num = num-1
+    return total
 
+print(otherFactorial(7))
 print(factorialStack(7))
