74da457ef5c9472a2151d114655d0e2ddbf95568
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-06-23 16:20:37 -0300

    recursiveItemSum

--- divideToConquist/recursiveSum.py
+++ divideToConquist/recursiveSum.py
@@ -4,6 +4,13 @@ def recursive_sum(lst):
     return lst.pop(0) + recursive_sum(lst)
 
 
-ls = [2, 4, 6]
+def recursive_item_sum(lst):
+    if len(lst) == 0:
+        return 0
+    return 1 + recursive_item_sum(lst[1:])
 
-print(recursive_sum(ls))
+
+ls1 = [2, 4, 6]
+ls2 = [1, 2, 3, 4, 5]
+print(recursive_sum(ls1))
+print(recursive_item_sum(ls2))
