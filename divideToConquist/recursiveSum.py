baa4d8de4a47c19ded61d043718a43083fa2e535
Vinicius Alkimin <viniciusg_alkimin@hotmail.com>
2024-06-25 23:21:10 -0300

    implementation of recursive biggest number

--- divideToConquist/recursiveSum.py
+++ divideToConquist/recursiveSum.py
@@ -10,7 +10,16 @@ def recursive_item_sum(lst):
     return 1 + recursive_item_sum(lst[1:])
 
 
+def recursive_biggest_item(lst):
+    if len(lst) == 2:
+        return lst[0] if lst[0] > lst[1] else lst[1]
+    sub_max = recursive_biggest_item(lst[1:])
+    return lst[0] if lst[0] > sub_max else sub_max
+
+
 ls1 = [2, 4, 6]
 ls2 = [1, 2, 3, 4, 5]
+ls3 = [8, 7, 10, 6, 9, 1, 3]
 print(recursive_sum(ls1))
 print(recursive_item_sum(ls2))
+print(recursive_biggest_item(ls3))
